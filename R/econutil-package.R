3de490316a5d8c4f1ac9adb410350d82b940ce14
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-05-02 13:57:59 +0900

    Reexport arguments()

--- R/econutil-package.R
+++ R/econutil-package.R
@@ -2,3 +2,11 @@
 #' @import vctrs
 #' @importFrom pillar obj_sum
 NULL
+
+#' @importFrom partialised arguments
+#' @export
+partialised::arguments
+
+#' @importFrom partialised `arguments<-`
+#' @export
+partialised::`arguments<-`
