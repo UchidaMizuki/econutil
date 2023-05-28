c0e022918688411c38fcc5c7b43a6a104cec0071
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-05-28 11:51:03 +0900

    Do not export `arguments()`

--- R/econutil-package.R
+++ R/econutil-package.R
@@ -2,11 +2,3 @@
 #' @import vctrs
 #' @importFrom pillar obj_sum type_sum
 NULL
-
-#' @importFrom partialised arguments
-#' @export
-partialised::arguments
-
-#' @importFrom partialised `arguments<-`
-#' @export
-partialised::`arguments<-`
